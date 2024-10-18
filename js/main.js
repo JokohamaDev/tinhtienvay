5d8661934bd86645f86c32d4a71a1c2f5ca1d727
JokohamaDev <jokohama1791@gmail.com>
2024-10-18 21:27:55 +0700

    Alpha 0.0.2 1. Added flex layout for result 2. Added input highlighted when empty 3. Added css color library

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 
+const inputs = document.querySelectorAll("#my-form input");
+    document.querySelector("#my-form input[type=button]").addEventListener("click", () => {
+        inputs.forEach(input => {
+            if (input.value.trim() === "") {
+                input.classList.add("input-error");
+            }
+        })
+    })
 
 function use_number(node) {
+    node.classList.remove("input-error");
     var empty_val = false;
     const value = node.value;
     if (node.value == '')
@@ -71,6 +80,19 @@ function calculate() {
             throw new Error("Invalid input values");
         }
 
+        if (amount <= 0) {
+            throw new Error("Amount must be greater than 0");
+        }
+
+        if (interest <= 0) {
+            throw new Error("Interest rate must be greater than 0");
+        }
+
+        if (tenor <= 0) {
+            throw new Error("Tenor must be greater than 0");
+        }
+
+        // Payment cố định theo số nợ ban đầu
         let interest_rate = interest / 1200;
         let monthly_payment = amount * Math.pow(1.0 + interest_rate, tenor) * interest_rate / (Math.pow(1.0 + interest_rate, tenor) - 1.0);
         let rounded_monthly = Math.round(monthly_payment) || 0;
@@ -80,24 +102,34 @@ function calculate() {
         let rounded_total = Math.round(total) || 0;
         total1Element.value = rounded_total.toLocaleString('en');
 
-        // Lãi suất dựa trên dư nợ giảm dần
+        // Payment giảm dần dựa trên dư nợ
         let total_decreasing = 0;
         let outstanding_balance = amount;
         for (let i = 0; i < tenor; i++) {
-            let monthly_payment_decreasing = outstanding_balance * interest/100;
-            total_decreasing += monthly_payment_decreasing;
-            outstanding_balance -= monthly_payment_decreasing;
+            let payment_decreasing = amount/tenor + outstanding_balance * interest/1200;
+            total_decreasing += payment_decreasing;
+            outstanding_balance -= payment_decreasing;
+            if (i == 0) {
+                let first_month_decreasing = payment_decreasing;
+                let rounded_first_month_decreasing = Math.round(first_month_decreasing) || 0;
+                firstMonthElement.value = rounded_first_month_decreasing.toLocaleString('en');
+            }
+            if (i == tenor-1) {
+                let last_month_decreasing = payment_decreasing;
+                let rounded_last_month_decreasing = Math.round(last_month_decreasing) || 0;
+                lastMonthElement.value = rounded_last_month_decreasing.toLocaleString('en');
+            }
         }
         let rounded_total_decreasing = Math.round(total_decreasing) || 0;
-        total2Element.value += '\n' + rounded_total_decreasing.toLocaleString('en');
+        total2Element.value = rounded_total_decreasing.toLocaleString('en');
 
-        let first_month_decreasing = amount * interest_rate;
-        let rounded_first_month_decreasing = Math.round(first_month_decreasing) || 0;
-        firstMonthElement.value += '\n' + rounded_first_month_decreasing.toLocaleString('en');
+        // let first_month_decreasing = amount * interest_rate;
+        // let rounded_first_month_decreasing = Math.round(first_month_decreasing) || 0;
+        // firstMonthElement.value += '\n' + rounded_first_month_decreasing.toLocaleString('en');
 
-        let last_month_decreasing = outstanding_balance * interest_rate;
-        let rounded_last_month_decreasing = Math.round(last_month_decreasing) || 0;
-        lastMonthElement.value += '\n' + rounded_last_month_decreasing.toLocaleString('en');
+        // let last_month_decreasing = outstanding_balance * interest_rate;
+        // let rounded_last_month_decreasing = Math.round(last_month_decreasing) || 0;
+        // lastMonthElement.value += '\n' + rounded_last_month_decreasing.toLocaleString('en');
 
     } catch (error) {
         console.error("Error calculating payment:", error.message);
